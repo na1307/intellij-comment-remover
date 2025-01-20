9be36a4da017703ee1cfc7d49a5bc1785434dac0
GitHub Action <action@github.com>
2025-01-20 05:37:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/na1307/intellijcommentremover/MyBundle.kt
+++ src/main/kotlin/com/github/na1307/intellijcommentremover/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.na1307.intellijcommentremover
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/na1307/intellijcommentremover/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/na1307/intellijcommentremover/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.na1307.intellijcommentremover.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
