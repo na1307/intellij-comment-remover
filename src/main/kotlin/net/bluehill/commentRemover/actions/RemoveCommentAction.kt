bc49cf2dac739b592edc735dc296a718abf964a9
Jonghyo Lee <na1307@outlook.kr>
2025-01-21 02:25:43 +0900

    Split

---
+++ src/main/kotlin/net/bluehill/commentRemover/actions/RemoveCommentAction.kt
@@ -0,0 +1,46 @@
+package net.bluehill.commentRemover.actions
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiComment
+import com.intellij.psi.util.PsiTreeUtil
+import net.bluehill.commentRemover.MyBundle
+
+abstract class RemoveCommentAction : AnAction() {
+    final override fun update(event: AnActionEvent) {
+        event.presentation.isEnabledAndVisible = event.project !== null
+    }
+
+    final override fun actionPerformed(event: AnActionEvent) {
+        val proj = event.project
+
+        if (proj == null) {
+            Messages.showErrorDialog(MyBundle.message("projectIsNull"), "Error")
+            return
+        }
+
+        val pf = event.getData(CommonDataKeys.PSI_FILE)
+
+        if (pf == null) {
+            Messages.showErrorDialog(proj, MyBundle.message("psiFileIsNull"), "Error")
+            return
+        }
+
+        val comments = PsiTreeUtil.collectElementsOfType(pf, PsiComment::class.java)
+
+        if (comments.isEmpty()) {
+            // Comments not found
+            return
+        }
+
+        removeComment(proj, comments)
+    }
+
+    abstract fun removeComment(proj: Project, comments: Iterable<PsiComment>)
+
+    final override fun getActionUpdateThread() = ActionUpdateThread.EDT
+}
---
+++ src/main/kotlin/net/bluehill/commentRemover/actions/RemoveSelectedCommentAction.kt
@@ -0,0 +1,47 @@
+package net.bluehill.commentRemover.actions
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+import net.bluehill.commentRemover.MyBundle
+
+class RemoveSelectedCommentAction : RemoveCommentAction() {
+    override fun removeComment(proj: Project, comments: Iterable<PsiComment>) {
+        val editor = FileEditorManager.getInstance(proj).selectedTextEditor
+
+        if (editor == null) {
+            Messages.showErrorDialog(proj, MyBundle.message("editorIsNull"), "Error")
+            return
+        }
+
+        val selection = findSelection(editor)
+
+        WriteCommandAction.runWriteCommandAction(proj) {
+            if (selection != null) {
+                // Removes comments from selected text.
+                comments.filter { it.isInSelectedLocation(selection) }.forEach { it.delete() }
+            } else {
+                // No selection: Do nothing.
+            }
+        }
+    }
+
+    private fun findSelection(editor: Editor): Pair<Int, Int>? {
+        val model = editor.selectionModel
+
+        return if (model.hasSelection()) model.selectionStart to model.selectionEnd else null
+    }
+
+    private fun PsiElement.isInSelectedLocation(pair: Pair<Int, Int>): Boolean {
+        // Get the range of the PsiElement
+        val elementStart = textRange.startOffset
+        val elementEnd = textRange.endOffset
+
+        // Check if the element's range is within the selection range
+        return elementStart >= pair.first && elementEnd <= pair.second
+    }
+}
